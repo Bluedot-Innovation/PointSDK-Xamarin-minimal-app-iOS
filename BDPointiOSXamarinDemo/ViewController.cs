c3de417505ffcb0cd69cb3e45813d2a46230796f
Duncan Lau <duncan@bluedotinnovation.com>
2018-03-22 14:11:31 +1100

    Initial commit

---
+++ BDPointiOSXamarinDemo/ViewController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Foundation;
+using libBDPointSDKiphoneos;
+using UIKit;
+
+namespace BDPointiOSXamarinDemo
+{
+    public partial class ViewController : UIViewController, IBDPSessionDelegate, IBDPLocationDelegate
+    {
+        BDLocationManager locationManager;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            updateLog("ViewDidload");
+			locationManager = BDLocationManager.Instance();
+
+			locationManager.SessionDelegate = this;
+			locationManager.LocationDelegate = this;
+
+            Authenticate.TouchUpInside += (o, s) => {
+                if (locationManager.AuthenticationState == BDAuthenticationState.Authenticated)
+                {
+                    Signout();
+                }
+                else
+                {
+                    StartAuthentication();
+                }
+			};
+
+        }
+
+		public void Signout()
+		{
+			if (locationManager.AuthenticationState == BDAuthenticationState.Authenticated)
+			{
+				locationManager.LogOut();
+			}
+			else
+			{
+				updateLog("Already Logged out");
+			}
+		}
+
+		private void StartAuthentication()
+		{
+			/* Start the Bluedot Point Service by providing with the credentials and a ServiceStatusListener, 
+             * the app will be notified via the status listener if the Bluedot Point Service started successful.
+             * 
+             * Parameters
+             * apiKey       The API key generated for your app in the Bluedot Point Access
+             * packageName  The package name of your app created in the Bluedot Point Access
+             * userName     The user name you used to login to the Bluedot Point Access
+             */
+			if (locationManager.AuthenticationState != BDAuthenticationState.Authenticated)
+			{
+				locationManager.AuthenticateWithApiKey("", "", "");
+			}
+			else
+			{
+				updateLog("Already Authenticated");
+			}
+		}
+
+		private void updateLog(String s)
+		{
+			String display = StatusLog.Text + "\n" + s;
+			StatusLog.Text = display;
+		}
+
+		public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+		[Export("didCheckIntoFence:inZone:atCoordinate:onDate:willCheckOut:withCustomData:")]
+		public void DidCheckIntoFence(BDFenceInfo fence, BDZoneInfo zoneInfo, BDLocationCoordinate2D coordinate, NSDate date, bool willCheckOut, NSDictionary customData)
+		{
+			updateLog("Checked into fence");
+		}
+
+		[Export("didCheckOutFromFence:inZone:onDate:withDuration:withCustomData:")]
+		public void DidCheckOutFromFence(BDFenceInfo fence, BDZoneInfo zoneInfo, NSDate date, nuint checkedInDuration, NSDictionary customData)
+		{
+			updateLog("Checked out from fence");
+		}
+
+		[Export("didCheckIntoBeacon:inZone:withProximity:onDate:willCheckOut:withCustomData:")]
+		public void DidCheckIntoBeacon(BDBeaconInfo beacon, BDZoneInfo zoneInfo, CoreLocation.CLProximity proximity, NSDate date, bool willCheckOut, NSDictionary customData)
+		{
+			updateLog("Checked into beacon");
+		}
+
+		[Export("didCheckOutFromBeacon:inZone:withProximity:onDate:withDuration:withCustomData:")]
+		public void DidCheckOutFromBeacon(BDBeaconInfo beacon, BDZoneInfo zoneInfo, CoreLocation.CLProximity proximity, NSDate date, nuint checkedInDuration, NSDictionary customData)
+		{
+			updateLog("Checked out from beacon");
+		}
+
+		public void WillAuthenticateWithUsername(string username, string apiKey, string packageName)
+		{
+			updateLog("Authenticating..");
+		}
+
+		public void AuthenticationWasSuccessful()
+		{
+			updateLog("Authentication was successful");
+            Authenticate.SetTitle("Logout", UIControlState.Normal);
+		}
+
+		public void AuthenticationWasDeniedWithReason(string reason)
+		{
+			updateLog("Authentication denied");
+            Authenticate.SetTitle("Authenticate", UIControlState.Normal);
+		}
+
+		public void AuthenticationFailedWithError(NSError error)
+		{
+			updateLog("Authentication failed");
+			Authenticate.SetTitle("Authenticate", UIControlState.Normal);
+		}
+
+		public void DidEndSession()
+		{
+			updateLog("Session ended");
+			Authenticate.SetTitle("Authenticate", UIControlState.Normal);
+		}
+
+		public void DidEndSessionWithError(NSError error)
+		{
+			updateLog("Session ended with error");
+			Authenticate.SetTitle("Authenticate", UIControlState.Normal);
+		}
+
+		[Export("didUpdateZoneInfo:")]
+		public void DidUpdateZoneInfo(NSSet zoneInfos)
+		{
+			updateLog("Zone Info updated");
+		}
+    }
+}
---
+++ BDPointiOSXamarinDemo/ViewController.designer.cs
@@ -0,0 +1,37 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace BDPointiOSXamarinDemo
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Authenticate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView StatusLog { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Authenticate != null) {
+                Authenticate.Dispose ();
+                Authenticate = null;
+            }
+
+            if (StatusLog != null) {
+                StatusLog.Dispose ();
+                StatusLog = null;
+            }
+        }
+    }
+}
